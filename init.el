ae3a96882bf6be25e531f2ec2714ce17b5f32494
Yuhuang Hu <duguyue100@gmail.com>
2016-10-01 14:37:21 +0200

    update magit

--- init.el
+++ init.el
@@ -1,3 +1,14 @@
 ;; My emacs configuration file
 ;; Author: Yuhuang Hu
 ;; Email : duguyue100@gmail.com
+
+;; For MELPA
+
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(package-initialize)
+
+;; Magit
+
+(global-set-key (kbd "C-x g") 'magit-status)
